30cc4921381d58e9cba64bcb2e744685e4ce8c63
CarmineMinichini <61963614+CarmineMinichini@users.noreply.github.com>
2021-02-18 17:08:09 +0100

    Add files via upload

---
+++ spyker_20/spyker.py
@@ -0,0 +1,40 @@
+import speech_recognition as sr
+from analysis import *
+# inizializza tokenizer
+recognizer_istance = sr.Recognizer()
+
+#tokenizer = nltk.data.load("/Users/carmineminichini/nltk_data/tokenizers/punkt/italian.pickle")
+
+# usi il Microphone come source
+with sr.Microphone() as source:
+    # elimina noise
+    recognizer_istance.adjust_for_ambient_noise(source)
+    # pause pause_threshold
+    recognizer_istance.pause_threshold = 3.0
+
+    print(".....  ....."*3)
+    print(""*2)
+    print("Dimmi pure..",end="\n")
+    # processing audio
+    audio = recognizer_istance.listen(source)
+    print("Ho capito..",end="\r")
+    print("Elaboro...",end="\r")
+
+
+
+try:
+    # recognizer ita
+    text = recognizer_istance.recognize_google(audio,language="it-IT")
+    result = '{}'.format(text)
+    with open("outputs.txt","w") as f:
+            f.write(result)
+    # riconosce il testo
+    print(" ")
+    print("---------Testo riconosciuto:--------")
+    print(text)
+    print(""*2)
+
+    analysis()
+    # Exception
+except Exception as e:
+    print(e)
